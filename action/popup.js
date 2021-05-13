aed9bf9fbb43d52d3d0ea9bffb1130618bd3850e
Ryoto <c0118128f5@edu.teu.ac.jp>
2021-05-13 19:38:47 +0900

    [Fix] Changed the name format of the output file

--- action/popup.js
+++ action/popup.js
@@ -25,9 +25,27 @@ action_button.addEventListener("click", async() => {
 
         let download_link = document.createElement('a');
         download_link.href = convert_to_objecturl;
-        download_link.download = 'tabs_list.json';
+        let today = formatDate(new Date(), 'YYYYMMDD_hhmmss')
+        download_link.download = 'tabs_list_' + today + '.json';
         download_link.click();
 
         URL.revokeObjectURL(convert_to_objecturl);
     });
 }); 
+
+
+function formatDate(date, format) {
+        
+    if (!format) {
+        format = 'YYYY/MM/DD hh:mm:ss';
+    };
+    
+    format = format.replace(/YYYY/g, date.getFullYear());
+    format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+    format = format.replace(/DD/g, ('0' + date.getDate()).slice(-2));
+    format = format.replace(/hh/g, ('0' + date.getHours()).slice(-2));
+    format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+    format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+    
+    return format;
+};
